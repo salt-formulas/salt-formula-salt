3f3a63b76fd31774ef1d1cf091bab2f84068fddc
Ales Komarek <mail@newt.cz>
2018-02-15 16:54:14 +0100

    Removed architect client from module
    Change-Id: I16a340051a2e1b2202b23f2fd3a6bab20b363ee8

--- _modules/architect.py
+++ _modules/architect.py
@@ -5,9 +5,7 @@ Salt modules to work with the Architect service.
 
 # Import python libs
 from __future__ import absolute_import
-import yaml
 import logging
-from architect_client.libarchitect import ArchitectClient
 
 __virtualname__ = 'architect'
 
@@ -16,60 +14,6 @@ logger = logging.getLogger(__name__)
 
 def __virtual__():
     return __virtualname__
-
-
-def _client():
-    return ArchitectClient()
-
-
-def inventory():
-    '''
-    Get the Architect metadata inventory
-
-    CLI Examples:
-
-    .. code-block:: bash
-
-        salt-call architect.inventory
-    '''
-    data = yaml.load(_client().get_data())
-
-    return data
-
-
-def node_pillar(name):
-    '''
-    Get the Architect node pillar for given Salt master.
-
-    CLI Examples:
-
-    .. code-block:: bash
-
-        salt-call architect.node_pillar node.domain
-    '''
-
-    data = yaml.load(_client().get_data(name))
-
-    return {
-        name: data
-    }
-
-
-def node_classify(name, data={}):
-    '''
-    CLassify node by given dictionary of parameters
-
-    CLI Examples:
-
-    .. code-block:: bash
-
-        salt-call architect.node_classify minion.net {'param1': 'value2'}
-    '''
-    output = _client().classify_node({
-        'name': name,
-        'data': data
-    })
-    return output
 
 
 def node_info():
